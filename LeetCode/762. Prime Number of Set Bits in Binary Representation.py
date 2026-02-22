b4bde949da00c8f53ef128b4d5f4091523c2829c
elsmirroad <elsmirroad@icloud.com>
2026-02-22 06:55:32 +0300

    upd: daily 52 update

--- LeetCode/762. Prime Number of Set Bits in Binary Representation.py
+++ LeetCode/762. Prime Number of Set Bits in Binary Representation.py
@@ -10,6 +10,11 @@ class Solution:
         return sum(True for num in range(left, right+1) if bin(num)[2:].count('1') in (2, 3, 5, 7, 11, 13, 17, 19))
 
 
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        return sum(num.bit_count() in {2, 3, 5, 7, 11, 13, 17, 19} for num in range(left, right+1))
+
+
 test("""
 Given two integers left and right, return the count of numbers in the inclusive range [left, right] having a prime number of set bits in their binary representation.
 Recall that the number of set bits an integer has is the number of 1's present when written in binary.
