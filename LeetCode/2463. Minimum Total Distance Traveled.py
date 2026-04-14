88f63c72f7d9354fae6133ae7c2831bc81dffc03
elsmirroad <elsmirroad@icloud.com>
2026-04-14 13:28:33 +0300

    daily 104

---
+++ LeetCode/2463. Minimum Total Distance Traveled.py
@@ -0,0 +1,90 @@
+from lc import *
+# ==============================================================
+# 2463. Minimum Total Distance Traveled
+# https://leetcode.com/problems/minimum-total-distance-traveled/
+# ==============================================================
+
+
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        n, m = len(robot), len(factory)
+        robot.sort()
+        factory.sort(key=lambda x: x[0])
+        dp = [[-1] * (m+1) for _ in range(n+1)]
+
+        def solve(i, j):
+            if i == n: return 0
+            if j == m: return inf
+            if dp[i][j] != -1: return dp[i][j]
+
+            ans = solve(i, j + 1)
+            total = 0
+            pos, cap = factory[j]
+
+            for k in range(cap):
+                if i + k >= n: break
+
+                total += abs(robot[i+k] - pos)
+
+                nxt = solve(i+k+1, j+1)
+
+                if nxt != inf:
+                    ans = min(ans, total + nxt)
+
+            dp[i][j] = ans
+            return ans
+
+        return solve(0, 0)
+
+
+test("""
+There are some robots and factories on the X-axis. You are given an integer array robot where robot[i] is the position of the i^th robot. You are also given a 2D integer array factory where factory[j] = [positionj, limitj] indicates that positionj is the position of the j^th factory and that the j^th factory can repair at most limitj robots.
+The positions of each robot are unique. The positions of each factory are also unique. Note that a robot can be in the same position as a factory initially.
+All the robots are initially broken; they keep moving in one direction. The direction could be the negative or the positive direction of the X-axis. When a robot reaches a factory that did not reach its limit, the factory repairs the robot, and it stops moving.
+At any moment, you can set the initial direction of moving for some robot. Your target is to minimize the total distance traveled by all the robots.
+Return the minimum total distance traveled by all the robots. The test cases are generated such that all the robots can be repaired.
+Note that
+
+All robots move at the same speed.
+If two robots move in the same direction, they will never collide.
+If two robots move in opposite directions and they meet at some point, they do not collide. They cross each other.
+If a robot passes by a factory that reached its limits, it crosses it as if it does not exist.
+If the robot moved from a position x to a position y, the distance it moved is |y - x|.
+
+ 
+Example 1:
+
+
+Input: robot = [0,4,6], factory = [[2,2],[6,2]]
+Output: 4
+Explanation: As shown in the figure:
+- The first robot at position 0 moves in the positive direction. It will be repaired at the first factory.
+- The second robot at position 4 moves in the negative direction. It will be repaired at the first factory.
+- The third robot at position 6 will be repaired at the second factory. It does not need to move.
+The limit of the first factory is 2, and it fixed 2 robots.
+The limit of the second factory is 2, and it fixed 1 robot.
+The total distance is |2 - 0| + |2 - 4| + |6 - 6| = 4. It can be shown that we cannot achieve a better total distance than 4.
+
+Example 2:
+
+
+Input: robot = [1,-1], factory = [[-2,1],[2,1]]
+Output: 2
+Explanation: As shown in the figure:
+- The first robot at position 1 moves in the positive direction. It will be repaired at the second factory.
+- The second robot at position -1 moves in the negative direction. It will be repaired at the first factory.
+The limit of the first factory is 1, and it fixed 1 robot.
+The limit of the second factory is 1, and it fixed 1 robot.
+The total distance is |2 - 1| + |(-2) - (-1)| = 2. It can be shown that we cannot achieve a better total distance than 2.
+
+ 
+Constraints:
+
+1 <= robot.length, factory.length <= 100
+factory[j].length == 2
+-10^9 <= robot[i], positionj <= 10^9
+0 <= limitj <= robot.length
+The input will be generated such that it is always possible to repair every robot.
+
+
+""")
