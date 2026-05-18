80c146b4c32869ea3494e7e6195d973878af3553
elsmirroad <elsmirroad@gmail.com>
2026-05-18 17:49:50 +0300

    daily 138

---
+++ LeetCode/1345. Jump Game IV.py
@@ -0,0 +1,76 @@
+from lc import *
+# ===========================================
+# 1345. Jump Game IV
+# https://leetcode.com/problems/jump-game-iv/
+# ===========================================
+
+
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        d = defaultdict(list)
+        for i, v in enumerate(arr):
+            d[v].append(i)
+
+        q = deque([0])
+        seen = [False] * n
+        seen[0] = True
+        step = 0
+
+        while q:
+            for _ in range(len(q)):
+                idx = q.popleft()
+                if idx == n-1: return step
+
+                if idx - 1 >= 0 and not seen[idx-1]:
+                    seen[idx-1] = True
+                    q.append(idx-1)
+                if idx + 1 < n and not seen[idx+1]:
+                    seen[idx+1] = True
+                    q.append(idx+1)
+
+                if arr[idx] in d:
+                    for neighbor in d[arr[idx]]:
+                        seen[neighbor] = True
+                        q.append(neighbor)
+                    del d[arr[idx]]
+            step += 1
+
+
+test("""
+Given an array of integers arr, you are initially positioned at the first index of the array.
+In one step you can jump from index i to index:
+
+i + 1 where: i + 1 < arr.length.
+i - 1 where: i - 1 >= 0.
+j where: arr[i] == arr[j] and i != j.
+
+Return the minimum number of steps to reach the last index of the array.
+Notice that you can not jump outside of the array at any time.
+ 
+Example 1:
+
+Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+Output: 3
+Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+
+Example 2:
+
+Input: arr = [7]
+Output: 0
+Explanation: Start index is the last index. You do not need to jump.
+
+Example 3:
+
+Input: arr = [7,6,9,6,9,6,9,7]
+Output: 1
+Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+
+ 
+Constraints:
+
+1 <= arr.length <= 5 * 10^4
+-10^8 <= arr[i] <= 10^8
+
+
+""")
