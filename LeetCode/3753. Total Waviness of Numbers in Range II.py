65a8382e92e0955358cb629d25ee77984114ecde
elsmirroad <elsmirroad@gmail.com>
2026-06-05 16:08:06 +0300

    daily 156

---
+++ LeetCode/3753. Total Waviness of Numbers in Range II.py
@@ -0,0 +1,96 @@
+from lc import *
+# ====================================================================
+# 3753. Total Waviness of Numbers in Range II
+# https://leetcode.com/problems/total-waviness-of-numbers-in-range-ii/
+# ====================================================================
+
+
+class Solution:
+    nums = []
+    for num in range(1000):
+        right = num % 10
+        mid = (num // 10) % 10
+        left = (num // 100) % 10
+        if (mid > max(left, right)) | (mid < min(left, right)):
+            nums.append(num)
+
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        return self.waveCount(num2) - self.waveCount(num1-1)
+
+    def waveCount(self, num):
+        if num < 100: return 0
+        return sum(self.countWays(num, p) for p in self.nums)
+
+    def countWays(self, num, pattern):
+        if num < 100: return 0
+        s = str(num)
+        n = len(s)
+        t = pattern < 100
+        total = 0
+
+        for i in range(n-2):
+            left = int(s[:i] or 0)
+            mid = int(s[i:i+3])
+            right = int(s[i+3:] or 0)
+            winds = 10 ** (n-i-3)
+            ways = 0
+            if mid > pattern: ways = left - t + 1
+            elif mid == pattern: ways = max(0, left-t); total += right + 1
+            else: ways = max(0, left-t)
+            total += ways * winds
+        return total
+
+
+test("""
+You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+The waviness of a number is defined as the total count of its peaks and valleys:
+
+A digit is a peak if it is strictly greater than both of its immediate neighbors.
+A digit is a valley if it is strictly less than both of its immediate neighbors.
+The first and last digits of a number cannot be peaks or valleys.
+Any number with fewer than 3 digits has a waviness of 0.
+
+Return the total sum of waviness for all numbers in the range [num1, num2].
+ 
+Example 1:
+
+Input: num1 = 120, num2 = 130
+Output: 3
+Explanation:
+In the range [120, 130]:
+
+120: middle digit 2 is a peak, waviness = 1.
+121: middle digit 2 is a peak, waviness = 1.
+130: middle digit 3 is a peak, waviness = 1.
+All other numbers in the range have a waviness of 0.
+
+Thus, total waviness is 1 + 1 + 1 = 3.
+
+Example 2:
+
+Input: num1 = 198, num2 = 202
+Output: 3
+Explanation:
+In the range [198, 202]:
+
+198: middle digit 9 is a peak, waviness = 1.
+201: middle digit 0 is a valley, waviness = 1.
+202: middle digit 0 is a valley, waviness = 1.
+All other numbers in the range have a waviness of 0.
+
+Thus, total waviness is 1 + 1 + 1 = 3.
+
+Example 3:
+
+Input: num1 = 4848, num2 = 4848
+Output: 2
+Explanation:
+Number 4848: the second digit 8 is a peak, and the third digit 4 is a valley, giving a waviness of 2.
+
+ 
+Constraints:
+
+1 <= num1 <= num2 <= 10^15​​​​​​​
+
+
+""")
