4466b880d24018bcc3d19013d69a57310a075187
elsmirroad <elsmirroad@gmail.com>
2026-07-10 11:29:58 +0300

    daily 191

---
+++ LeetCode/3534. Path Existence Queries in a Graph II.py
@@ -0,0 +1,145 @@
+from lc import *
+# ===================================================================
+# 3534. Path Existence Queries in a Graph II
+# https://leetcode.com/problems/path-existence-queries-in-a-graph-ii/
+# ===================================================================
+
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[int]:
+        default = [(v, i) for i, v in enumerate(nums)]; default.sort()
+
+        point = {}
+        for i, (v, node) in enumerate(default):
+            point[node] = i
+
+        jumps = [0] * n
+        for i, (v, node) in enumerate(default):
+            nxt = bisect.bisect_left(default, (v+maxDiff, inf)) - 1
+            jumps[i] = nxt
+
+        LOG = n.bit_length()
+        up = [jumps]
+
+        for _ in range(1, LOG):
+            last = up[-1]
+            up.append([last[last[i]] for i in range(n)])
+
+        result = []
+        for u, v in queries:
+            u = point[u]
+            v = point[v]
+            if u == v: result.append(0); continue
+            if u > v: u, v = v, u
+            curr = u
+            currJumps = 0
+
+            for k in range(LOG-1, -1, -1):
+                if up[k][curr] < v:
+                    curr = up[k][curr]
+                    currJumps += 2**k
+            if jumps[curr] >= v: result.append(currJumps+1)
+            else: result.append(-1)
+
+        return result
+
+
+test("""
+You are given an integer n representing the number of nodes in a graph, labeled from 0 to n - 1.
+You are also given an integer array nums of length n and an integer maxDiff.
+An undirected edge exists between nodes i and j if the absolute difference between nums[i] and nums[j] is at most maxDiff (i.e., |nums[i] - nums[j]| <= maxDiff).
+You are also given a 2D integer array queries. For each queries[i] = [ui, vi], find the minimum distance between nodes ui and vi. If no path exists between the two nodes, return -1 for that query.
+Return an array answer, where answer[i] is the result of the i^th query.
+Note: The edges between the nodes are unweighted.
+ 
+Example 1:
+
+Input: n = 5, nums = [1,8,3,4,2], maxDiff = 3, queries = [[0,3],[2,4]]
+Output: [1,1]
+Explanation:
+The resulting graph is:
+
+
+
+
+Query
+Shortest Path
+Minimum Distance
+
+
+[0, 3]
+0 → 3
+1
+
+
+[2, 4]
+2 → 4
+1
+
+
+
+Thus, the output is [1, 1].
+
+Example 2:
+
+Input: n = 5, nums = [5,3,1,9,10], maxDiff = 2, queries = [[0,1],[0,2],[2,3],[4,3]]
+Output: [1,2,-1,1]
+Explanation:
+The resulting graph is:
+
+
+
+
+
+Query
+Shortest Path
+Minimum Distance
+
+
+[0, 1]
+0 → 1
+1
+
+
+[0, 2]
+0 → 1 → 2
+2
+
+
+[2, 3]
+None
+-1
+
+
+[4, 3]
+3 → 4
+1
+
+
+
+Thus, the output is [1, 2, -1, 1].
+Example 3:
+
+Input: n = 3, nums = [3,6,1], maxDiff = 1, queries = [[0,0],[0,1],[1,2]]
+Output: [0,-1,-1]
+Explanation:
+There are no edges between any two nodes because:
+
+Nodes 0 and 1: |nums[0] - nums[1]| = |3 - 6| = 3 > 1
+Nodes 0 and 2: |nums[0] - nums[2]| = |3 - 1| = 2 > 1
+Nodes 1 and 2: |nums[1] - nums[2]| = |6 - 1| = 5 > 1
+
+Thus, no node can reach any other node, and the output is [0, -1, -1].
+
+ 
+Constraints:
+
+1 <= n == nums.length <= 10^5
+0 <= nums[i] <= 10^5
+0 <= maxDiff <= 10^5
+1 <= queries.length <= 10^5
+queries[i] == [ui, vi]
+0 <= ui, vi < n
+
+
+""")
