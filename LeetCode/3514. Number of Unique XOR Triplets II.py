850d924ad377d97aca6cba95a808dbad7079c558
elsmirroad <elsmirroad@gmail.com>
2026-07-24 17:21:22 +0300

    daily 205

---
+++ LeetCode/3514. Number of Unique XOR Triplets II.py
@@ -0,0 +1,57 @@
+from lc import *
+# ===============================================================
+# 3514. Number of Unique XOR Triplets II
+# https://leetcode.com/problems/number-of-unique-xor-triplets-ii/
+# ===============================================================
+
+
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        seen, result = set(), set()
+
+        for i in range(n):
+            for j in range(i, n):
+                seen.add(nums[i] ^ nums[j])
+
+        for i in range(n):
+            for pair in seen:
+                result.add(nums[i] ^ pair)
+
+        return len(result)
+
+
+test("""
+You are given an integer array nums.
+A XOR triplet is defined as the XOR of three elements nums[i] XOR nums[j] XOR nums[k] where i <= j <= k.
+Return the number of unique XOR triplet values from all possible triplets (i, j, k).
+ 
+Example 1:
+
+Input: nums = [1,3]
+Output: 2
+Explanation:
+The possible XOR triplet values are:
+
+(0, 0, 0) → 1 XOR 1 XOR 1 = 1
+(0, 0, 1) → 1 XOR 1 XOR 3 = 3
+(0, 1, 1) → 1 XOR 3 XOR 3 = 1
+(1, 1, 1) → 3 XOR 3 XOR 3 = 3
+
+The unique XOR values are {1, 3}. Thus, the output is 2.
+
+Example 2:
+
+Input: nums = [6,7,8,9]
+Output: 4
+Explanation:
+The possible XOR triplet values are {6, 7, 8, 9}. Thus, the output is 4.
+
+ 
+Constraints:
+
+1 <= nums.length <= 1500
+1 <= nums[i] <= 1500
+
+
+""")
